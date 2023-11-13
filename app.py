b52652b0518323d2286e44be05ff392cc2e1a7f9
shilnaprdisa <shilnaparadisa6@gmail.com>
2023-11-13 23:00:21 +0700

    menambah use case

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def Hello():
+    return "Hello, World!"
+
+@app.route("/profil")
+def profil():
+    return render_template ('profile.html')
+
+if __name__=="__main__":
+        app.run()
