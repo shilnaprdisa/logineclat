0034fe1e5dcbb4f2d54584ec0dad0203a5b43083
shilnaprdisa <shilnaparadisa6@gmail.com>
2023-12-08 07:49:20 +0700

    tambah database

--- db.py
+++ db.py
@@ -1,1 +1,11 @@
 #database
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database ="eclatdatabase",
+    password="",
+)
+if conn.is_connected():
+    print("terhubung")
